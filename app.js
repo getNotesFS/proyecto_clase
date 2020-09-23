b67d9dd4ec8b3b84f72fb5ab779e1147f7a7cd97
getNotesFS <sfmarmolg@gmail.com>
2020-09-22 23:23:50 -0500

    0.1 Proyecto Clase

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//MONGOSE BASE DE DATOS
+require('./app_server/models/db');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ app_server/models/db.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+
+//Localhost debería de cambiar por el servidor en el que está la base de datos
+const dbURI = 'mongodb://localhost/bigpizzadb';
+mongoose.connect(dbURI, { useNewUrlParser: true });
+
+//MULTIPLE DATA BASE
+const dbURIlog = 'mongodb://localhost/mibasededatosLog';
+const logDB = mongoose.createConnection(dbURIlog);
+
+const readLine = require('readline');
+
+if (process.platform === 'win32') {
+   const rl = readLine.createInterface({
+      input: process.stdin,
+      output: process.stdout
+   });
+   rl.on('SIGINT', () => {
+      process.emit("SIGINT");
+   });
+}
+
+//CLOSE
+const procShutdown = (msg, callback) => {
+   mongoose.connection.close(() => {
+      console.log(`Mongoose disconnected through ${msg}`);
+      callback();
+   });
+};
+
+//CALL PROCSHUTDOWN
+// Evento SIGUSR2 - nodemon
+process.once('SIGUSR2', () => {
+   procShutdown('nodemon restart', () => {
+      process.kill(process.pid, 'SIGUSR2');
+   });
+});
+// Evento SIGINT - windows
+process.on('SIGINT', () => {
+   procShutdown('app windows termination', () => {
+      process.exit(0);
+   });
+});
+// Evento SIGTERM - heroku
+process.on('SIGTERM', () => {
+   procShutdownnodemon('Heroku app shutdown', () => {
+      process.exit(0);
+   });
+});
+
+
+//MONITOREO DB1========================
+//VERIFICACION DE CONEXIÓN
+
+mongoose.connection.on('connected', () => {
+   console.log(`Mongoose se conectó a ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+   console.log('Mongoose error de conexión:', err);
+});
+mongoose.connection.on('disconnected', () => {
+   console.log('Mongoose desconectado');
+});
+
+//MONITOREO DB2 =================================
+
+// DB conectada
+logDB.on('connected', () => {
+   console.log(`Mongoose log se conectó a  ${dbURIlog}`);
+});
+// Error de conexion
+logDB.on('error', err => {
+   console.log("Mongoose log error de conexión: ", err);
+});
+// Desconexión
+logDB.on('disconnected', () => {
+   console.log('Mongoose log está desconectado');
+});
+
+// Cerrar la conexion
+// logDB.close(() => {
+//    console.log('Mongoose log cerrada');
+// });
+
+
+require('./locations');
